521a92b9306fde43a0dcf42d65cdb8a26e0ba63a
amine-ghazoui <ghazouimohamedamine@gmail.com>
2024-11-26 00:09:22 +0100

    javafx setup

---
+++ TP5/src/exercice_1/IMetier.java
@@ -0,0 +1,15 @@
+package exercice_1;
+
+import java.util.List;
+
+public interface IMetier <T>{
+
+    public void add(T o);
+
+    public List<T> getAll();
+
+    public T findById(long id);
+
+    public void delete(long id);
+
+}
